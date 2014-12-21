2a9c6cfad0e6e26bfc5a7a06c6af1eed6b50d81e
Marshall <marshall.burchard@mac.com>
2014-12-21 12:12:59 -0500

    First Cut

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix creates a matrix object with an iniital value equal to the
+## passed value, and a cached copy (originally set to NULL) is overwritten
+## with the initial value if different from the initial value
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve returns the cached value of the inverse of x if x is a new
+## matrix, otherwise it calculates, caches, and returns the inverse of x
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
+
+testm <- matrix(1:9,3,3)
+testn <- matrix(1:25,5,5)
+
+BigMat <- makeCacheMatrix(testm)
+BiggerMatrix <- makeCacheMatrix(testn)
+
+cacheSolve(BigMat)
